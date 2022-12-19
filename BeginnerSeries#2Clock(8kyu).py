dc8c05d82e2322ce8191ee0d64565ea46e90d43a
krzys194 <61507827+krzys194@users.noreply.github.com>
2022-12-20 00:27:35 +0100

    Commit

--- BeginnerSeries#2Clock(8kyu).py
+++ BeginnerSeries#2Clock(8kyu).py
@@ -2,6 +2,6 @@
 # https://www.codewars.com/kata/55f9bca8ecaa9eac7100004a/python
 
 def past(h, m, s):
-    return s*1000+m*60000+h*3600000
+    return s * 1000 + m * 60000 + h * 3600000
 
 past(1,0,1)
