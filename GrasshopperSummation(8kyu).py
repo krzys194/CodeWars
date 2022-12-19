3f2d3b722c18a089eb74b70053047ae46945b235
krzys194 <61507827+krzys194@users.noreply.github.com>
2022-12-20 00:21:53 +0100

    Commit

---
+++ GrasshopperSummation(8kyu).py
@@ -0,0 +1,11 @@
+# link do zadania:
+# https://www.codewars.com/kata/55d24f55d7dd296eb9000030/python
+
+def summation(num):
+    n = 0
+    for i in range(0, num+1):
+        n += i 
+    return n
+    
+
+summation(100)
